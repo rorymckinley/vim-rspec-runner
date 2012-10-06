7f73b53cfb3bf4695b0c63dc49f6e749cb4600fb
Rory McKinley <rorymckinley@gmail.com>
2012-10-06 09:35:36 +0200

    Find the executable

--- autoload/rspecrunner.vim
+++ autoload/rspecrunner.vim
@@ -5,3 +5,8 @@ endfunction
 function! rspecrunner#PathToFormatter()
   return g:plugin_location . "/formatter/vim_quickfix_formatter.rb"
 endfunction
+
+function! rspecrunner#GetExecutable()
+  system("bundle list | grep 'rspec (2'")
+  return v:shell_error == 0 ? "rspec" : "spec"
+endfunction
--- spec/rspec-runner_spec.rb
+++ spec/rspec-runner_spec.rb
@@ -15,4 +15,9 @@ describe "spec runner plugin" do
     path_to_formatter = File.expand_path(File.join(path_to_plugin, 'plugin', 'formatter', 'vim_quickfix_formatter.rb'))
     @vim.command("echo rspecrunner#PathToFormatter()").should eq path_to_formatter
   end
+
+  it "checks for the correct executable of rspec" do
+    # TODO: before you're done figure out if you want this to be more granular - e.g. rspec 1 checks
+    @vim.command("echo rspecrunner#GetExecutable()").should eq "rspec"
+  end
 end
